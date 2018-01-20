8eb4e3a708ff4057ce0d20f5a5e6cc8c1905a4a9
KIeran McClymont <kieraninnewzealand@hotmail.com>
2018-01-20 17:55:16 +1300

    Added more functionality to the timer + introduced a selector box to chooes how often the bell rings as a reminder.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import bell from './Sounds/Bell.wav'
 
 
 let fakeUser = {
@@ -28,44 +29,79 @@ class MenuBar extends Component {
 }
 
 class OptionsMenu extends Component {
+  constructor(){
+    super()
+    this.state = {
+      reminder: 0,
+    }
+  }
+
+  componentDidMount(){
+    let event = document.getElementById('timerSelect')
+    event.addEventListener('change', e => {
+      this.setState({reminder: event.options[event.selectedIndex].value})
+      this.props.connector(this.state.reminder)
+    })
+  }
+
   render() {
     return(
       <div className='optionsBar'>
-        Options
+        <h1>Options</h1>
+        <div>
+          <select id='timerSelect'>
+            <option value=''>Muted</option>
+            <option value='60'>1 min reminder</option>
+            <option value='300'>5 min reminder</option>
+            <option value='600'>10 min reminder</option>
+            <option value='3600'>60 min reminder</option>
+          </select>
+          <select id='visualTimer'>
+            <option value='1'>yes</option>
+            <option value='0'>no</option>
+          </select>
+        </div>
       </div>
     )
   }
 }
 
+
 class DigitalTime extends Component {
-  render() {
-    return(
-      <h2>Digital Timer</h2>
-    )
-  }
-}
-
-class AnalogTime extends Component {
-
-
   constructor() {
     super();
     this.state = {
       seconds: 0,
-      minutes: 0
+      minutes: 0,
+      longestTime: 0,
+      counter: 0
     };
   }
 
 
   handleStartClick() {
+    const timer = document.querySelector('.secondsHand')
+    const audio = document.querySelector('.bell')
     this.incrementer = setInterval(() => {
-      this.setState({seconds:(this.state.seconds + 1)})
+      timer.style.animation = 'spinner 1s ease-out infinite'
+      this.setState({
+        counter: this.props.counterState,
+        seconds:(this.state.seconds + 1),
+        minutes: Math.floor(this.state.seconds / 60)
+      })
+      if (this.state.seconds%this.state.counter === 0){
+        audio.play()
+      }
     }, 1000);
   };
 
   handleStopClick() {
+    const timer = document.querySelector('.secondsHand')
+    if (this.state.seconds > this.state.longestTime){
+      this.setState({longestTime: this.state.seconds})
+    }
     clearInterval(this.incrementer)
-
+    timer.style.animation = '';
   };
 
   getSeconds() {
@@ -82,51 +118,70 @@ class AnalogTime extends Component {
       minutes: 0
     })
   }
+
+  longestTimeHandler() {
+    if (this.state.longestTime > 60){
+      return Math.floor(this.state.longestTime /60) + ":" +
+      ('0' + this.state.longestTime % 60).slice(-2);
+    } else {
+      return ('0' + this.state.longestTime % 60).slice(-2)
+    }
+  }
+
   render() {
 
     return (
-      <div>
+      <div className='digitalTimer'>
+        <h1>Timer</h1>
+        <div className='clock'>
+          <div className='secondsHand'><div className='hand'></div></div>
+        </div>
         <h1>{this.getMinutes()} : {this.getSeconds()}</h1>
-        <button type='button' onClick={this.handleStartClick.bind(this)}>
-          Start
-        </button>
-        <button type='button' onClick={this.handleStopClick.bind(this)}>
-          Stop
-        </button>
-        <button type='button' onClick={this.reset.bind(this)}>
-          Reset
-        </button>
+        <div className='timerButtonContainer'>
+          <button type='button' onClick={this.handleStartClick.bind(this)}>
+            Start
+          </button>
+          <button type='button' onClick={this.handleStopClick.bind(this)}>
+            Stop
+          </button>
+          <button type='button' onClick={this.reset.bind(this)}>
+            Reset
+          </button>
+        </div>
+        <h2>Longest Time: {this.longestTimeHandler()} </h2>
+        <audio src={bell} className='bell'></audio>
       </div>
     )
   }
 }
 
-
-
-
-
 class App extends Component {
   constructor() {
     super()
-    this.state = {serverData: fakeUser}
+    this.state = {
+      serverData: fakeUser,
+      counter: 0,
+    }
   }
+
+  connector(data) {
+    this.setState({counter : data})
+  }
+
   render() {
     return (
       <div>
         {this.state.serverData ?
           <div>
             <MenuBar/>
-            <OptionsMenu/>
+            <OptionsMenu connector={this.connector.bind(this)}/>
             <div className='timerContainer'>
-              <AnalogTime/>
-              <DigitalTime/>
+              <DigitalTime counterState={this.state.counter}/>
             </div>
           </div>
           : []}
       </div>
     )
-
-
   }
 }
 
