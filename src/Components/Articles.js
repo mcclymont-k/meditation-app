7d4334a812ca70c667ce1eb2e0b5b00ba80e0f30
Kieran McClymont <mcclymont.kieran@gmail.com>
2018-03-14 17:30:31 -0300

    <Updated the Articles Component to loop using a map(), entered dummy data and styled each article.

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 
-
 class Articles extends Component {
-  render() {
-    return(
+  constructor() {
+    super()
+    this.state = {
+      articles: [
+        {
+          name: 'Article 1',
+          image: 'href',
+          blurb: 'QWERtyuiop',
+        },
+        {
+          name: 'Article 2',
+          image: 'href',
+          blurb: 'QWERtyuiop',
+        },
+        {
+          name: 'Article 3',
+          image: 'href',
+          blurb: 'QWERtyuiop',
+        },
+      ]
+    }
+  }
+
+  renderArticles(article) {
+    return (
       <div>
         <div className='articleContainer'>
+          <div className='articleLeft'>
+            <div className='articleName'><p>{article.name}</p></div>
+            <div className='articleImage'>{article.image}</div>
+          </div>
+          <div className='articleRight'>
+            <h1>Blurb</h1>
+            {article.blurb}
+          </div>
+        </div>
+        <div className='borderBottom'></div>
+      </div>
+    )
+  }
 
+  render() {
+
+
+    return (
+      <div>
+        <div className='articlesContainer'>
+          {this.state.articles.map(article =>
+            this.renderArticles(article)
+            )
+          }
         </div>
       </div>
+
     )
   }
 }
