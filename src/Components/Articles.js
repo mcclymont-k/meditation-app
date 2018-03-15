e2f24232e5d53366286a683f005788cc5d6a6251
Kieran McClymont <mcclymont.kieran@gmail.com>
2018-03-15 09:05:16 -0300

    Added fake data and completed map loop to manage article data.

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -6,19 +6,22 @@ class Articles extends Component {
     this.state = {
       articles: [
         {
-          name: 'Article 1',
-          image: 'href',
-          blurb: 'QWERtyuiop',
+          name: 'Vipassana Is the Craziest Shit You ll Ever Do.',
+          image: 'https://thelinknewspaper.ca/images/made/images/articles/Volume_37/Ops/_resized/16.OPS.Meditation.ZoeGelfant_900_1245_90.jpg',
+          blurb: 'Voluntarily signing up for a prison of silence is one thing, but actually following through with it takes a lot of courage.',
+          source: 'https://thelinknewspaper.ca/article/vipassana-meditation-is-the-craziest-shit-youll-ever-do'
         },
         {
           name: 'Article 2',
           image: 'href',
           blurb: 'QWERtyuiop',
+          source: ''
         },
         {
           name: 'Article 3',
           image: 'href',
           blurb: 'QWERtyuiop',
+          source: ''
         },
       ]
     }
@@ -29,11 +32,10 @@ class Articles extends Component {
       <div>
         <div className='articleContainer'>
           <div className='articleLeft'>
-            <div className='articleName'><p>{article.name}</p></div>
-            <div className='articleImage'>{article.image}</div>
+            <div className='articleName'><a href={article.source}>{article.name}</a></div>
+            <img className='articleImage' src={article.image}></img>
           </div>
           <div className='articleRight'>
-            <h1>Blurb</h1>
             {article.blurb}
           </div>
         </div>
