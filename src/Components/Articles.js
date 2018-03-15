4f3d0180c27134850aebfdd8b131dd604789f6be
Kieran McClymont <mcclymont.kieran@gmail.com>
2018-03-15 09:45:04 -0300

    More article data and updated styling

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -12,16 +12,16 @@ class Articles extends Component {
           source: 'https://thelinknewspaper.ca/article/vipassana-meditation-is-the-craziest-shit-youll-ever-do'
         },
         {
-          name: 'Article 2',
-          image: 'href',
-          blurb: 'QWERtyuiop',
-          source: ''
+          name: 'How do I start meditating?',
+          image: "https://www.theglobeandmail.com/resizer/UB3eh2paW3d67U0jomUosF6gJ9o=/620x0/filters:quality(80)/arc-anglerfish-tgam-prod-tgam.s3.amazonaws.com/public/X53FIN6HXBEK7A4N2A7WGOYSPU.jpg",
+          blurb: "Meditation is said to relieve stress, improve concentration, increase happiness and even slow aging! Sign me up, I thought, happily. It's the elixir I've been searching for.",
+          source: "https://www.theglobeandmail.com/life/health-and-fitness/health/how-do-i-start-meditating/article37700869/"
         },
         {
-          name: 'Article 3',
-          image: 'href',
-          blurb: 'QWERtyuiop',
-          source: ''
+          name: "Meditation and yoga can 'reverse' DNA reactions which cause stress, new study suggests.",
+          image: "https://www.sciencedaily.com/images/2017/06/170615213301_1_540x360.jpg",
+          blurb: "Mind-body interventions (MBIs) such as meditation, yoga and Tai Chi don't simply relax us; they can 'reverse' the molecular reactions in our DNA which cause ill- health and depression, according to a study.",
+          source: 'https://www.sciencedaily.com/releases/2017/06/170615213301.htm'
         },
       ]
     }
